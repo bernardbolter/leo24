523a77cf7c8be96622f214f04bb08347416651e5
Besco <bernardbolter@gmail.com>
2024-02-26 22:48:48 +0100

    thumbs working

--- src/components/Thumb.jsx
+++ src/components/Thumb.jsx
@@ -10,7 +10,27 @@ const Thumb = ({ thumb, index }) => {
     const controls = useAnimationControls()
 
     useEffect(() => {
+        if (index < leo.currentImageIndex) {
+            console.log('less: ', index)
+            controls.start({
+                maskPosition: '0% 0%',
+                transition: { duration: 0 }
+            })
+            // controls.set({
+            //     maskPosition: '0% 0% !important'
+            // })
+        }
+        if (index > leo.currentImageIndex) {
+            console.log("more: ", index)
+            controls.start({
+                maskPosition: '100% 100%',
+                transition: { duration: 0}
+            })
+        }
         if (index === leo.currentImageIndex) {
+            controls.set({
+                maskPosition: '100% 100%'
+            })
             controls.start({
                 maskPosition: '0% 0%',
                 transition: { duration: parseInt(thumb.video_length) }
@@ -20,18 +40,25 @@ const Thumb = ({ thumb, index }) => {
             controls.stop()
         }
 
-    }, [leo.currentImageIndex, leo.aboutOpen, leo.infoOpen])
+    }, [index, leo.currentImageIndex, leo.aboutOpen, leo.infoOpen])
 
     return (
         <section 
             className="project-thumb"
             onClick={() => {
+                console.log(index)
                 setLeo(state => ({ ...state, currentImageIndex: index }))
             }}
         >
             <motion.div
+                initial={{ maskPosition: '100% 100%'}}
                 animate={controls}
-                className="thumb-overlay" 
+                // onAnimationComplete={() => {
+                //     console.log("ani complete: ", index)
+                //     setLeo(state => ({ ...state, currentImageIndex: index + 1}))
+                // }}
+                className="thumb-overlay"
+                // className={index < leo.currentImageIndex ? 'thumb-overlay thumb-overlay-on' : 'thumb-overlay'} 
             />
             <Image
                 src={thumb.image ? thumb.image.sizes.thumbnail : thumb.thubmnail ? thumb.thumbnail.sizes.thumbnail : 'https://www.tlbx.app/200-300.svg'}
